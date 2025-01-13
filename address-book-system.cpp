38976f0f1061652a2616dc02b20fca95d258a39b
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-01-13 17:31:52 +0530

    [Harsimran] update : updating a file

--- address-book-system.cpp
+++ address-book-system.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-class Contacts{
+class Contacts {
 public:
     string first_name;
     string last_name;
@@ -12,32 +14,110 @@ public:
     long phone_number;
     string email;
 
-    Contacts(string first_name, string last_name, string address, string city, int zip_number, long phone_number, string email){
+    Contacts(string first_name, string last_name, string address, string city, int zip_number, long phone_number, string email) {
         this->first_name = first_name;
-        this->last_name =  last_name;
+        this->last_name = last_name;
         this->address = address;
         this->city = city;
         this->zip_number = zip_number;
         this->phone_number = phone_number;
-        this->email = email;        
+        this->email = email;
     }
 
-    void getInfo(){
-    cout << "first_name : " << first_name << endl;
-    cout << "last_name : " << last_name << endl;
-    cout << "address : " << address << endl;
-    cout << "city : " << city << endl;
-    cout << "zip_number : " << zip_number << endl;
-    cout << "phone_number : " << phone_number << endl;
-    cout << "email : " << email << endl;
+    void getInfo() {
+        cout << "First Name: " << first_name << endl;
+        cout << "Last Name: " << last_name << endl;
+        cout << "Address: " << address << endl;
+        cout << "City: " << city << endl;
+        cout << "Zip Number: " << zip_number << endl;
+        cout << "Phone Number: " << phone_number << endl;
+        cout << "Email: " << email << endl;
     }
 };
 
-int main(){
+class AddressBook {
+    vector<Contacts> contacts;
+
+public:
+    void addContact(const Contacts& contact) {
+        contacts.push_back(contact); 
+    }
+
+    void displayContacts() {
+        for (size_t i = 0; i < contacts.size(); ++i) {
+            contacts[i].getInfo(); 
+        }
+    }
+
+    void editContact(string first_name, string last_name) {
+        for (size_t i = 0; i < contacts.size(); ++i) {
+            if (contacts[i].first_name == first_name && contacts[i].last_name == last_name) {
+                string newAddress, newCity, newEmail;
+                int newZip;
+                long newPhone;
+
+                cout << "Enter new Address: ";
+                cin.ignore();
+                getline(cin, newAddress);
+                cout << "Enter new City: ";
+                cin >> newCity;
+                cout << "Enter new Zip Number: ";
+                cin >> newZip;
+                cout << "Enter new Phone Number: ";
+                cin >> newPhone;
+                cout << "Enter new Email: ";
+                cin >> newEmail;
+
+                
+                Contacts updatedContact(first_name, last_name, newAddress, newCity, newZip, newPhone, newEmail);
+                contacts[i] = updatedContact;
+
+                return;
+            }
+        }
+        cout << "Contact not found!" << endl;
+    }
+};
+
+int main() {
     cout << "Welcome to Address Book" << endl;
-    Contacts c1("simran", "kaur", "Sangrur, Punjab", "Patiala", 9087, 98675545, "harsimran@gmail.com");
-    c1.getInfo();
+
+    AddressBook a1; 
+
+    string first_name, last_name, address, city, email;
+    int zip_number;
+    long phone_number;
+
+    cout << "Enter First Name: ";
+    cin >> first_name;
+    cout << "Enter Last Name: ";
+    cin >> last_name;
+    cout << "Enter Address: ";
+    cin.ignore();
+    getline(cin, address);
+    cout << "Enter City: ";
+    cin >> city;
+    cout << "Enter Zip Number: ";
+    cin >> zip_number;
+    cout << "Enter Phone Number: ";
+    cin >> phone_number;
+    cout << "Enter Email: ";
+    cin >> email;
+
+    Contacts newContact(first_name, last_name, address, city, zip_number, phone_number, email);
+    a1.addContact(newContact); 
+
+    a1.displayContacts();
+
+    cout << "Enter First Name: ";
+    cin >> first_name;
+    cout << "Enter Last Name: ";
+    cin >> last_name;
+
+    a1.editContact(first_name, last_name);
+
+    cout << "Update contacts" << endl;
+    a1.displayContacts();
 
     return 0;
-    
 }
