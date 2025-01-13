08f0979aeb47d5a6f88fdb00ca8c6c83ac78c065
Harsimran Kaur <103635368+Harsimran06Grewal@users.noreply.github.com>
2025-01-13 23:37:16 +0530

    Update address-book-system.cpp

--- address-book-system.cpp
+++ address-book-system.cpp
@@ -50,7 +50,7 @@ public:
     }
 
     void editContact(string first_name, string last_name) {
-        for (size_t i = 0; i < contacts.size(); ++i) {
+        for (int i = 0; i < contacts.size(); i++) {
             if (contacts[i].first_name == first_name && contacts[i].last_name == last_name) {
                 string newAddress, newCity, newEmail;
                 int newZip;
@@ -79,7 +79,7 @@ public:
     }
 
     void deleteContact(string first_name, string last_name) {
-        for (size_t i = 0; i < contacts.size(); ++i) {
+        for (int i = 0; i < contacts.size(); i++) {
             if (contacts[i].first_name == first_name && contacts[i].last_name == last_name) {
                 contacts.erase(contacts.begin() + i);  // Removes the contact at index i
                 cout << "Contact deleted successfully!" << endl;
